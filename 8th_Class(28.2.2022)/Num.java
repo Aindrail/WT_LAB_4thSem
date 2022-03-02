ebce6b9cef9599f6fe109ef18e520bcfa0852837
Aindrail <aindrailsantra@gmail.com>
2022-03-02 19:22:44 +0530

    No. of object

--- 8th_Class(28.2.2022)/Num.java
+++ 8th_Class(28.2.2022)/Num.java
@@ -1,16 +1,16 @@
-public class Num 
-{
-    static int n = 0;
-    Num()
-    {
-        n++;
-    }
-    public static void main(String[] args)
-    {
-      Num obj1 = new Num();
-      Num obj2 = new Num();
-      Num obj3 = new Num();
-      Num obj4 = new Num();
-      System.out.println("Number of objects created  : " + n);
-    }
-}
+// public class Num 
+// {
+//     static int n = 0;
+//     Num()
+//     {
+//         n++;
+//     }
+//     public static void main(String[] args)
+//     {
+//       Num obj1 = new Num();
+//       Num obj2 = new Num();
+//       Num obj3 = new Num();
+//       Num obj4 = new Num();
+//       System.out.println("Number of objects created  : " + n);
+//     }
+// }
