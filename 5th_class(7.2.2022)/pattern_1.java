31fa8ffacb3a7708129b4402c96f1f046315a5de
Aindrail <aindrailsantra@gmail.com>
2022-03-02 19:18:51 +0530

    Minor Changes

--- 5th_class(7.2.2022)/pattern_1.java
+++ 5th_class(7.2.2022)/pattern_1.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
  class pattern_1 {
      public static void main(String[] args){
--- 6th_class(14.2.2022)/no_of_object.java
+++ 6th_class(14.2.2022)/no_of_object.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class no_of_object {
+// import java.util.*;
+// public class no_of_object {
 
-    static int c = 0;
-  {
-      c += 1;
-  }
-  public static void main (String[] args)
-  {
-      no_of_object t1 = new no_of_object();
-      no_of_object t2 = new no_of_object();
-      no_of_object t3 = new no_of_object();
-      no_of_object t4 = new no_of_object();
-      no_of_object t5 = new no_of_object();
-      no_of_object t6 = new no_of_object();
-      no_of_object t7 = new no_of_object();
-      no_of_object t8 = new no_of_object();
-      System.out.println("Number of objects created : " + c);
-  } 
-}
+//     static int c = 0;
+//   {
+//       c += 1;
+//   }
+//   public static void main (String[] args)
+//   {
+//       no_of_object t1 = new no_of_object();
+//       no_of_object t2 = new no_of_object();
+//       no_of_object t3 = new no_of_object();
+//       no_of_object t4 = new no_of_object();
+//       no_of_object t5 = new no_of_object();
+//       no_of_object t6 = new no_of_object();
+//       no_of_object t7 = new no_of_object();
+//       no_of_object t8 = new no_of_object();
+//       System.out.println("Number of objects created : " + c);
+//   } 
+// }
