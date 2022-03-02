248c6ca587c912ed41a5098c6821ff3174dc488d
Aindrail <aindrailsantra@gmail.com>
2022-03-02 19:17:07 +0530

    8th Assignment Q1.Q2

--- 8th_Class(28.2.2022)/Area.java
+++ 8th_Class(28.2.2022)/Area.java
@@ -1,29 +1,29 @@
-public class Area
- {
-    double res;
-    Area(int r)
-    {
-    res = 3.14 * r * r;
-    }
-    Area(int b, int h)
-    {
-        res = (b * h)/2;
-    }
-    Area (int l, double w)
-    {
-        res = l * w;
-    }
-    void disp()
-    {
-        System.out.println("Area is " + res);
-    }
-    public static void main(String[] args)
-    {
-       Area obj = new Area(5);
-       obj.disp();
-       Area obj1 = new Area(5, 6);
-       obj1.disp();
-       Area obj2 = new Area(7, 8.0);
-       obj2.disp();
-    }
-}
+// public class Area
+//  {
+//     double res;
+//     Area(int r)
+//     {
+//     res = 3.14 * r * r;
+//     }
+//     Area(int b, int h)
+//     {
+//         res = (b * h)/2;
+//     }
+//     Area (int l, double w)
+//     {
+//         res = l * w;
+//     }
+//     void disp()
+//     {
+//         System.out.println("Area is " + res);
+//     }
+//     public static void main(String[] args)
+//     {
+//        Area obj = new Area(5);
+//        obj.disp();
+//        Area obj1 = new Area(5, 6);
+//        obj1.disp();
+//        Area obj2 = new Area(7, 8.0);
+//        obj2.disp();
+//     }
+// }
