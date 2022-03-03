07b44e99c384c4de1ab2d355932f29d6d96fdbc8
Aindrail <aindrailsantra@gmail.com>
2022-03-03 18:13:58 +0530

    Transpose Matrix

--- 8th_Class(28.2.2022)/Transpose.java
+++ 8th_Class(28.2.2022)/Transpose.java
@@ -1,45 +1,45 @@
-//  import java.util.*;
-// public class Transpose 
-// {
-//     public static void main(String []args)
-//     {
-//         Scanner sc = new Scanner(System.in);
-//         int a[][] = new int[3][3];
-//         for (int i = 0; i < 3; i++)
-//         {
-//             for (int j = 0; j < 3; j++)
-//             {
-//             System.out.println("Enter the number");
-//             a[i][j] = sc.nextInt();
-//             }
-//         }
-//         System.out.println("Matrix");
-//         for (int i = 0; i < 3; i++)
-//         {
-//             for (int j = 0; j < 3; j++)
-//             {
-//             System.out.print("  " + a[i][j]);
-//             }
-//             System.out.println();
-//         }
-//         for (int i = 0; i < 3; i++)
-//         {
-//             for (int j = 0; j < 3; j++)
-//             {
-//                 int t = a[j][i];
-//                 a[j][i] = a[i][j];
-//                 a[i][j] =t;
-//             }
-//         }
-//       System.out.println("Transpose");
-//       for (int i = 0; i < 3; i++)
-//       {
-//           for (int j = 0; j < 3; j++)
-//           {
-//           System.out.print("  " + a[j][i]);
-//           }
-//           System.out.println();
-//       }
-//       sc.close();
-// }
-// }
+ import java.util.*;
+public class Transpose 
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[3][3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+            System.out.println("Enter the number");
+            a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+            System.out.print("  " + a[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                int t = a[j][i];
+                a[j][i] = a[i][j];
+                a[i][j] =t;
+            }
+        }
+      System.out.println("Transpose");
+      for (int i = 0; i < 3; i++)
+      {
+          for (int j = 0; j < 3; j++)
+          {
+          System.out.print("  " + a[j][i]);
+          }
+          System.out.println();
+      }
+      sc.close();
+}
+}
